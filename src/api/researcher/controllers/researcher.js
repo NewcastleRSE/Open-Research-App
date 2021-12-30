f9f04ffdf63a3c2885c38886c7d18a1f1078ba41
Soldham1 <s.oldham1204@gmail.com>
2021-12-30 16:36:31 +0000

    Set up content-types in Strapi

---
+++ src/api/researcher/controllers/researcher.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  researcher controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::researcher.researcher');
---
+++ src/api/researcher/routes/researcher.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * researcher router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::researcher.researcher');
