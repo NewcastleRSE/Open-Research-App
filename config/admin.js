dda912bfb07c1f3c3a07a3c88864ca47f96cd2c7
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-04-11 10:03:10 +0100

    Updated file with my local changes

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', "2'&Pz:`\et{eEnLdp'bdW.&F"),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '3b0c993312109e6794989ee2c3c35c78'),
   },
